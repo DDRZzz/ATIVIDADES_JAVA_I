7bb6e23265db8cbc3ac3355542ec1ab7deec76c8
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-20 08:45:06 -0300

    Create Q1.java

---
+++ ATIVIDADE I/Q1.java
@@ -0,0 +1,14 @@
+package com.mycompany.testedoctorz;
+
+// 1. Escrever um programa para exibir os números de 1 até 50 na tela. 
+
+public class ContarNumeros {
+    public static void main (String [] args){
+        
+        for (int num = 1;num <= 50;num ++){
+            
+            System.out.println(num);   
+        }
+    } 
+    
+}
