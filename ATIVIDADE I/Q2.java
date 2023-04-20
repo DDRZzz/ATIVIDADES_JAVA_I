687a19a8aa642ee29d677f812a53c7e15cc75e31
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-20 09:25:53 -0300

    Create Q2.java

---
+++ ATIVIDADE I/Q2.java
@@ -0,0 +1,14 @@
+//2. Fazer um programa para encontrar todos os números pares entre 1 e 100.
+
+public class Pares1E100 {
+
+	public static void main(String[] args) {
+		
+		for(int num = 0; num <= 100;num++) {
+				if (num%2 == 0) {
+						System.out.println(num);
+				}
+		}
+	}
+
+}
