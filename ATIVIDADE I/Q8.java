a55114bac9cb3d7bbd5f8dc63d9b138c0c39dc80
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-24 23:31:10 -0300

    Create Q8.java

---
+++ ATIVIDADE I/Q8.java
@@ -0,0 +1,12 @@
+/*8. Escrever um programa para exibir os números de 1 até 50 na tela. */
+
+public class Q8 {
+    public static void main(String [] args) {
+        int k = 1;
+
+        while(k <= 50){
+            System.out.println(k);
+            k++;
+        }
+    }
+}
