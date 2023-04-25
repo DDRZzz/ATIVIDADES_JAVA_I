e1e34ec28662dc8fd1a848cde68a970db9ba53ec
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-24 23:06:12 -0300

    Create Q5.java

---
+++ ATIVIDADE I/Q5.java
@@ -0,0 +1,18 @@
+//5. Ler 10 números e imprimir quantos são pares e quantos são ímpares.
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+        int num, qtdi = 0;
+        for (int i = 0; i< 10; i++){
+            System.out.println("Digite o número da posição "+i);
+            num = input.nextInt();
+            if(num % 2 == 1){
+                qtdi++;
+            }
+        }
+        System.out.println("A quantidade de números ímpares é: "+qtdi);
+    }
+}
