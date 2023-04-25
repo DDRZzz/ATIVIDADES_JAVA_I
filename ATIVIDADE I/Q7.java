e8c1501e3f21cd24d864d33881ab41bb697b34e3
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-24 23:26:38 -0300

    Create Q7.java

---
+++ ATIVIDADE I/Q7.java
@@ -0,0 +1,13 @@
+/*7. Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma
+estrutura sequencial e uma estrutura de repetição while. */
+
+public class Q7 {
+    public static void main (String[] args){
+        
+            int i = 0;
+            while (i < 11){
+                System.out.println("Hello World");
+                i++;
+            }
+        }
+}
