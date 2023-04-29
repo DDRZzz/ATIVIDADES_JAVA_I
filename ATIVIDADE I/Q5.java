a5f9862639fcc0b20c765ab794c406ff0ed1ba35
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-04-29 20:02:27 -0300

    Update Q5.java

--- ATIVIDADE I/Q5.java
+++ ATIVIDADE I/Q5.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Q5 {
     public static void main (String [] args){
         Scanner input = new Scanner (System.in);
-        int num, qtdi = 0;
+        int num, qtdi = 0,qtdp = 0;
         for (int i = 0; i< 10; i++){
             System.out.println("Digite o número da posição "+i);
             num = input.nextInt();
             if(num % 2 == 1){
                 qtdi++;
+            }else{
+                qtdp++;
             }
         }
         System.out.println("A quantidade de números ímpares é: "+qtdi);
+        System.out.println("\nA quantidade de números pares é: "+qtdp);
     }
 }
