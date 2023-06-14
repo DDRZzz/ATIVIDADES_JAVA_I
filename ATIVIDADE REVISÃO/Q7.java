cf8c93ec4eb860acc54bc8ae14a92b740d82b48d
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-06-14 11:24:02 -0300

    Create Q7.java

---
+++ ATIVIDADE REVISÃO/Q7.java
@@ -0,0 +1,19 @@
+package br.ifsertaope.cs.av.revisao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class conta_corrente {
+    
+    public void depositar (){
+        
+    }
+    public void sacar (){
+        
+    }
+    public void saldo (){
+        
+    }
+    
+}
